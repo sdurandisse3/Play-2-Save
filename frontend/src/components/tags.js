d2f007ba07127e345c80e15b0183857a925f8ea6
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-05-18 12:18:28 -0400

    added component folders&tag state

---
+++ frontend/src/components/tags.js
@@ -0,0 +1,30 @@
+import react from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Tags extends React.Component {
+   constructor(props) {
+       super (props)
+
+       this.state= {
+           user: null,
+           category: null,
+           type: null,
+           number: null
+       }
+   };
+
+   componentDidMount () {
+
+   }
+
+   componentDidUpdate () {
+
+   }
+   render() {
+       return(
+           <div></div>
+       )
+   }
+}
+
+export default Tags
