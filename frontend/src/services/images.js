4e3425b4f13bd5dfa53b440f7da2c9024212ab65
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-18 16:51:22 -0400

    set what u get from local storage

--- frontend/src/services/images.js
+++ frontend/src/services/images.js
@@ -1,6 +1,8 @@
 const ImageService = {}
 
 ImageService.init = () => {
-    if (localStorage.getItem('images') === null) 
+    if (localStorage.getItem('images') === null) {
+      localStorage.setItem('images', JSON.stringify([]));
+    }
   }
 export default ImageService;
