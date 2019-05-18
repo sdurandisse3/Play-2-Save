9407b0cd630d25b79ad8124a8aeff896edda5656
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-17 23:33:22 -0400

    handlesubmit

--- frontend/src/containers/signup.js
+++ frontend/src/containers/signup.js
@@ -11,7 +11,19 @@ class SignUp extends React.Component{
       handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
       }
-
+      handleSubmit = (e) => {
+        e.preventDefault();
+    
+        const { email, password } = this.state;
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((response) => {
+            console.log('Returns: ', response);
+          })
+          .catch(err => {
+            const { message } = err;
+            this.setState({ error: message });
+          })
+      }
 
       render() {
       
