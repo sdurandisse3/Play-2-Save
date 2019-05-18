ef603ef84bdcf4228b52307be01168032a41f9e0
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-17 22:29:11 -0400

    stated state

--- frontend/src/containers/signup.js
+++ frontend/src/containers/signup.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class SignUp extends React.Component{
-
+    state = {
+        email: '',
+        password: '',
+        error: ''
+      }
+    
 }
 export default SignUp;
