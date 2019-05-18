6f417e73bf5960a0614eeb1ffd5432c820543984
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-05-18 12:56:48 -0400

    added routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import SignUp from './containers/signup';
+import {HashRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+import Home from './containers/home';
+import Snappers from './containers/snappers';
 
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-      <SignUp/>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <HashRouter>
+        <Route path='/*' component= {Navbar} />
+        <Switch>
+
+        <Route path='/' exact component= {Home}/>
+        <Route path='/snappers' exact component={Snappers} />
+        </Switch>
+      </HashRouter>
+    );
+  };
 }
+  
 
 export default App;
