47b6e9df7463e8a03887f6f5d70151b5e51cc574
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-18 16:50:07 -0400

    initiated img service

--- frontend/src/components/feed.js
+++ frontend/src/components/feed.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from '../components/image';
+import Image from './components/image';
 import ImageService from '../services/images';
 
 
---
+++ frontend/src/services/images.js
@@ -0,0 +1,2 @@
+const ImageService = {}
+export default ImageService;
