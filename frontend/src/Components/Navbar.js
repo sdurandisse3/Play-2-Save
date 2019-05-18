86f781f4eeb2d4299ac5e9205045db50dd1a5ea3
AaronTeele <aaronteele@AaronTeeles-Air.fios-router.home>
2019-05-18 12:22:04 -0400

    updated navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -39,35 +39,3 @@ export default Navbar;
 
 
 
-
-// <nav className="navbar navbar-expand-lg navbar-light bg-light nav-justified">
-        //     <Link className="navbar-brand" to="#">SnapShopper</Link>
-        //     <button
-        //         className="navbar-toggler"
-        //         type="button"
-        //         data-toggle="collapse"
-        //         data-target="#navbarNavDropdown"
-        //         aria-controls="navbarNavDropdown"
-        //         aria-expanded="false" aria-label="Toggle navigation"
-        //     >
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-        //     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        //         <ul className="navbar-nav ">
-        //             <li className="nav-item active">
-        //                 <Link className="nav-link" to="#">Home</Link>
-        //             </li>
-
-        //             <li>
-        //                 {/* <Link to={Camera}> */}
-        //                 
-        //                 {/* </Link> */}
-        //             </li>
-
-        //             <li className="nav-item">
-        //                 <Link className="nav-link" to="#">Categories</Link>
-        //             </li>
-
-        //         </ul>
-        //     </div>
-        // </nav>
