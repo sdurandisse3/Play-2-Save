fc6610c822afc00b8a176a811019c7a3acdd98e3
Malcolm Barbee <MalcolmBarbee@pursuit.com>
2019-05-19 13:15:15 -0400

    initial commit

---
+++ frontend/src/components/home.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends react.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                Home Page
+                </div>
+        )
+    }
+}
+
+export default Home;
