8b6521a456f96fc0f365f7563eaf552be8b8a10d
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-19 18:21:19 -0400

    if st

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -4,7 +4,9 @@ import {withRouter} from 'react-router-dom'
 
 const Button = (props) =>{
     const {goTo,value, handleOnClick} = props  //goTo is the url to go to and value is the value of the button
-    return <button type="button" className="btn btn-primary" onClick={e=>
+    return <button type="button" className="btn btn-primary" onClick={e=>{
+        if(value === 'My Location') handleOnClick()
+        props.history.push(goTo)}
     }></button>
 }
 
