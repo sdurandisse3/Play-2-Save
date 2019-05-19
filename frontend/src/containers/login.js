7a7bac8b9a6ac8847be2b4a4380423a9190e0b38
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-19 11:30:59 -0400

    added form

--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -14,6 +14,19 @@ class Login extends React.Component{
         const displayForm = <>
       <h1>Login</h1>
       {displayError}
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Login</button>
+      </form>
+      </>;
+
           return(
 
           )
