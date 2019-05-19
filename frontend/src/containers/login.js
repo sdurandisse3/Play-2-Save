a02fc4278e40a6075e4bb442901a557e709058a0
Ivy Strickland <IvyStrickland@pursuit.org>
2019-05-19 11:29:58 -0400

    added display form

--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -11,6 +11,7 @@ class Login extends React.Component{
       render(){
         const { email, password, error } = this.state;
         const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
+        const displayForm = <>
 
           return(
 
