8916f89ba6c565fc15f271ea8fdd18145a15ba0b
AaronTeele <a_teele@yahoo.com>
2019-05-26 22:00:20 -0400

    updated icon and navbar styling

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,61 +1,63 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AuthContext from '../contexts/auth';
+import './header.css';
 
 
 const Header = (props) => {
-return(
-<AuthContext.Consumer>
-            {
-              (user) => {
-                if (user) {
-                  return (    
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">Snap Shoppers</Link>
-    <ul className="navbar-nav">  
-      <li className="nav-item">
-        <Link className="nav-link" to="/camera">Camera</Link>
-      </li> 
-      <li className="nav-item">
-        <Link className="nav-link" to="/feed">Feed</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/upload">Upload Coupon</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/tags">Tags</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/logout">Logout</Link>
-      </li>
-    </ul>
-  </nav>  
-
-                  )
-
-                } else {
-                  return (
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                  <Link className="navbar-brand" to="/">Snap Shoppers</Link>
-                  <ul className="navbar-nav">
+  return (
+    <AuthContext.Consumer>
+      {
+        (user) => {
+          if (user) {
+            return (
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">Snap Shoppers</Link>
+                <ul className="navbar-nav">
                   <li className="nav-item">
-                      <Link className="nav-link" to="/login">Login</Link>
-                    </li>  
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/signup">Sign Up</Link>
-                    </li>    
-                  </ul>
-                </nav>           
-                  )
-          
-                }
-              }
-            }
-          </AuthContext.Consumer>
+                    <Link className="nav-link" to="/feed">Feed</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/upload">Upload Coupon</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/camera">
+                      <i class="fas fa-barcode fa-3x"></i>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/tags">Tags</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/logout">Logout</Link>
+                  </li>
+                </ul>
+              </nav>
+            )
+
+          } else {
+            return (
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">Snap Shoppers</Link>
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                  </li>
+                </ul>
+              </nav>
+            )
+
+          }
+        }
+      }
+    </AuthContext.Consumer>
 
 
-      
 
-)
+
+  )
 }
 export default Header;
